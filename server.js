cec49ea760f8acdd3a7f7d03270befcbc1f544d8
Dimitar Mateev <mateevpro@gmail.com>
2017-06-21 19:32:52 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express= require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  fs.appendFile('./server.log', `${now}: ${req.method} ${req.url}\n`, (err) => {
+    if (err) console.log('Unable to write data to log');
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my site'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fetch that page'
+  });
+});
+
+app.listen(3000);
